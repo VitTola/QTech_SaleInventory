113ae9da8bbd80aea1f820a065891f1a667e63ce
TOLA <vittola.tiger@gmail.com>
2021-10-15 11:24:41 +0700

    add updaterDialog

---
+++ QTech.App/Updater/AboutUsDialog.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace QTech.Updater
+{
+    partial class AboutUsDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.exPanel = new QTech.Component.Components.ExPanel();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnClose = new QTech.Component.ExButtonLoading();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this._lblComanyName = new System.Windows.Forms.Label();
+            this.container.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.exPanel.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // container
+            // 
+            this.container.Controls.Add(this._lblComanyName);
+            this.container.Controls.Add(this.panel1);
+            this.container.Controls.Add(this.exPanel);
+            this.container.Size = new System.Drawing.Size(452, 270);
+            // 
+            // picLogo
+            // 
+            this.picLogo.BackColor = System.Drawing.Color.Transparent;
+            this.picLogo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picLogo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picLogo.ErrorImage = global::QTech.Properties.Resources.QTech__2_;
+            this.picLogo.Image = global::QTech.Properties.Resources.Pheng_Ry;
+            this.picLogo.InitialImage = global::QTech.Properties.Resources.QTech__2_;
+            this.picLogo.Location = new System.Drawing.Point(0, 0);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(450, 71);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 1;
+            this.picLogo.TabStop = false;
+            this.picLogo.WaitOnLoad = true;
+            // 
+            // exPanel
+            // 
+            this.exPanel.BackColor = System.Drawing.Color.Transparent;
+            this.exPanel.Controls.Add(this.flowLayoutPanel3);
+            this.exPanel.Controls.Add(this.flowLayoutPanel2);
+            this.exPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.exPanel.Location = new System.Drawing.Point(1, 233);
+            this.exPanel.Name = "exPanel";
+            this.exPanel.Padding = new System.Windows.Forms.Padding(2);
+            this.exPanel.Size = new System.Drawing.Size(450, 36);
+            this.exPanel.TabIndex = 23;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.btnClose);
+            this.flowLayoutPanel3.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(288, 2);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(160, 32);
+            this.flowLayoutPanel3.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(2, 2);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(106, 32);
+            this.flowLayoutPanel2.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.DefaultImage = null;
+            this.btnClose.Executing = false;
+            this.btnClose.Location = new System.Drawing.Point(83, 3);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2, 3, 2, 2);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.ShortcutAligment = QTech.Component.ExButtonLoading.Aligment.Horizontal;
+            this.btnClose.ShortcutText = null;
+            this.btnClose.Size = new System.Drawing.Size(75, 27);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "បិទ";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.picLogo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(450, 71);
+            this.panel1.TabIndex = 25;
+            // 
+            // _lblComanyName
+            // 
+            this._lblComanyName.AutoSize = true;
+            this._lblComanyName.BackColor = System.Drawing.Color.Transparent;
+            this._lblComanyName.Cursor = System.Windows.Forms.Cursors.Hand;
+            this._lblComanyName.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this._lblComanyName.Font = new System.Drawing.Font("Khmer Muol", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._lblComanyName.ForeColor = System.Drawing.Color.Blue;
+            this._lblComanyName.Location = new System.Drawing.Point(142, 75);
+            this._lblComanyName.Name = "_lblComanyName";
+            this._lblComanyName.Size = new System.Drawing.Size(161, 24);
+            this._lblComanyName.TabIndex = 26;
+            this._lblComanyName.Text = "ផេង រី ផ្គត់ផ្គង់ខ្សាច់និងថ្ម";
+            // 
+            // AboutUsDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 290);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "AboutUsDialog";
+            this.Text = "AboutUsDialog";
+            this.container.ResumeLayout(false);
+            this.container.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.exPanel.ResumeLayout(false);
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picLogo;
+        private Component.Components.ExPanel exPanel;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private Component.ExButtonLoading btnClose;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label _lblComanyName;
+    }
+}
---
+++ QTech.App/Updater/AboutUsDialog.cs
@@ -0,0 +1,21 @@
+﻿using QTech.Component;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QTech.Updater
+{
+    public partial class AboutUsDialog : ExDialog
+    {
+        public AboutUsDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
